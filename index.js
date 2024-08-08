77b47bb511a0215d52eee22c39fc1d847fa1348e
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:25:32 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Get started!
+
+const name = prompt("What's your name?")
+console.log(`Welcome to my game,  ${name}`)
+const Num = 8
+let ranNUm = prompt("Pick a random number from 1-10")
+
+if (ranNUm == 8) {
+  console.log("Congrats you got it!")
+}else{
+  console.log("Wrong, try again")
+  let ranNUm = prompt("Pick a random number from 1-10")
+  
+}
